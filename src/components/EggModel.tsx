b46f48bc52866235bbc0c4ccee1f4251e24762eb
Aydin Terzi <aydinterzi7@gmail.com>
2025-02-05 20:34:13 +0300

    realistic egg

--- src/components/EggModel.tsx
+++ src/components/EggModel.tsx
@@ -1,9 +1,11 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
 
 function EggModel() {
   const mesh = useRef();
-
+  const model = useGLTF("/egg.glb");
+  console.log(model);
   useFrame((state, delta) => {
     if (mesh.current) {
       mesh.current.rotation.y += delta * 0.5;
@@ -11,9 +13,8 @@ function EggModel() {
   });
 
   return (
-    <mesh ref={mesh}>
-      <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial color="white" />
+    <mesh ref={mesh} scale={0.7}>
+      <primitive object={model.scene} />;
     </mesh>
   );
 }
